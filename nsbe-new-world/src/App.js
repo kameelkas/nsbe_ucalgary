100f53371fe653a64b39770c26b9e50fc05f7dce
Kameel Kasumu <kameelkas@gmail.com>
2024-08-31 16:03:47 -0600

    Background Color, Skeleton Member

--- nsbe-new-world/src/App.js
+++ nsbe-new-world/src/App.js
@@ -1,16 +1,18 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Event from './Event'
 import Members from './Member'
+import { Box } from '@chakra-ui/react'
+
 
 function App() {
   return (
       <Router>
-        <div minH="100vh">
+        <Box minHeight="100vh" bg='brand.NSBEBlack'>
           <Routes>
             <Route path="/events" element={<Event />} />
             <Route path="/members" element={<Members />} />
           </Routes>
-        </div>
+        </Box>
       </Router>
   );
 }
