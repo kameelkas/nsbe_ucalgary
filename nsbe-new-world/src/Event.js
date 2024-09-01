614cd05e301b66b5dd48bcf44290dade649f64d0
Kameel Kasumu <kameelkas@gmail.com>
2024-08-31 22:56:16 -0600

    Members Page Done!

--- nsbe-new-world/src/Event.js
+++ nsbe-new-world/src/Event.js
@@ -1,7 +1,9 @@
 import React from "react";
+import { Flex, Box, Image, Text, SimpleGrid } from '@chakra-ui/react';
+
 function Event() {
   console.log("Event component rendered!");
-  return <div>EVENTS</div>;
+  return <Box color='white'>EVENTS</Box>;
 }
 
 export default Event;
