f1daa069f8ee7474d069af06d6511eb67af29325
fisayoadabs <fisayoadabs@gmail.com>
2024-09-01 19:09:53 -0600

    Update 3: Added subtle animation to contact page

--- nsbe-new-world/src/App.js
+++ nsbe-new-world/src/App.js
@@ -3,6 +3,7 @@ import Event from './Event'
 import Members from './Member'
 import { Box } from '@chakra-ui/react'
 import Contact from './Contact';
+import HomePage from './Home';
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <Router>
       <Box minHeight="100vh" bg='brand.NSBEBlack'>
         <Routes>
+          <Route path="/" element={<HomePage/>}/>
           <Route path="/events" element={<Event />} />
           <Route path="/members" element={<Members />} />
           <Route path="/contact" element={<Contact />} />
